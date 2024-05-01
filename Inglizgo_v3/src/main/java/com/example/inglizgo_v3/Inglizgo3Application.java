9982a212dbde7979e063757194d2f93e7f95bbfb
Muhab Endish <134897223+muhabend@users.noreply.github.com>
2024-05-01 23:26:19 +0300

    Update Inglizgo3Application.java
    updated on 5/1/2024

--- Inglizgo_v3/src/main/java/com/example/inglizgo_v3/Inglizgo3Application.java
+++ Inglizgo_v3/src/main/java/com/example/inglizgo_v3/Inglizgo3Application.java
@@ -3,6 +3,7 @@ package com.example.inglizgo_v3;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,8 +14,13 @@ public class Inglizgo3Application extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Inglizgo3Application.class.getResource("Login_SignUp_ForotPassword.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 650, 500);
-        stage.setTitle("Inglizgo v3");
+        stage.setTitle("Inglizgo");
         stage.setScene(scene);
+
+        // Load and set the application icon
+        //Image icon = new Image(""); // Replace "path_to_your_icon.png" with the path to your icon
+        //stage.getIcons().add(icon);
+
         stage.show();
     }
 
