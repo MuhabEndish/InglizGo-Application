9f2d387bea50d22c89ab219428e1f54493193763
Muhab Endish <134897223+muhabend@users.noreply.github.com>
2024-05-01 23:25:22 +0300

    Update AlertMessage.java
    updated on 5/1/2024

--- Inglizgo_v3/src/main/java/com/example/inglizgo_v3/AlertMessage.java
+++ Inglizgo_v3/src/main/java/com/example/inglizgo_v3/AlertMessage.java
@@ -1,6 +1,9 @@
 package com.example.inglizgo_v3;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
 
 public class AlertMessage {
 
@@ -24,6 +27,14 @@ public class AlertMessage {
         alert.setHeaderText(null);
         alert.setContentText(message);
         alert.showAndWait();
+    }
+
+    public Optional<ButtonType> confirmationMessage(String message) {
+        alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation message");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        return alert.showAndWait();
     }
 
 }
